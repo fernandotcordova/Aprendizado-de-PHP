2c8346831a113c285983368f9838d993f0b599ff
= <=>
2025-08-09 22:27:04 -0300

    Adicionando o repositório do curso ao git

---
+++ ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    </ <meta charset=" UTF-8">
+    <title>Exercício 1 PHP - Se livrando da maldição</title>
+</head>
+
+<body>
+    <h1>
+        <?php
+            print "Olá, mundo! \u{1F30E}";
+        ?>
+    </h1>
+    <p>Vou me livrar da maldição</p>
+
+</body>
+
+</html>
